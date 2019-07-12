1e1c699f00801d3d0a45380e2a06174d75625761
LordKorea <lk97798@posteo.net>
2019-07-13 00:44:27 +0200

    added ticket update formatting

--- src/main/java/io/gitlab/lordkorea/simpletickets/model/TicketUpdate.java
+++ src/main/java/io/gitlab/lordkorea/simpletickets/model/TicketUpdate.java
@@ -1,5 +1,12 @@
 package io.gitlab.lordkorea.simpletickets.model;
 
+import io.gitlab.lordkorea.simpletickets.SimpleTicketsPlugin;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.bukkit.Bukkit;
+
+import java.util.UUID;
+
 /**
  * An update to a ticket's status.
  */
@@ -11,55 +18,77 @@ public class TicketUpdate extends TicketHistoryEntry {
     private final Type updateType;
 
     /**
+     * The issuer of the update.
+     */
+    private final UUID issuer;
+
+    /**
+     * Any additional data to the update.
+     */
+    private final String data;
+
+    /**
      * Constructor.
      *
      * @param timestamp  The timestamp of this status update.
      * @param updateType The type of update of this entry.
+     * @param issuer     The UUID of the player who caused this update.
+     * @param data       Any additional data to the update.
      */
-    public TicketUpdate(final long timestamp, final Type updateType) {
+    public TicketUpdate(final long timestamp, final Type updateType, final UUID issuer, final String data) {
         super(timestamp);
         this.updateType = updateType;
+        this.issuer = issuer;
+        this.data = data;
     }
 
     @Override
     public String getDisplayString() {
-        // TODO use i18n file for this
-        return null;
+        final String timestamp = getFormattedTimestamp();
+        final String issuerName = Bukkit.getOfflinePlayer(issuer).getName();
+        return SimpleTicketsPlugin.getMessageManager().format("simpletickets.format.comment", timestamp,
+                issuerName, data);
     }
 
     /**
      * The different types of ticket update.
      */
+    @RequiredArgsConstructor
     public enum Type {
 
         /**
          * This update is generated when a ticket is created.
          */
-        CREATED,
+        CREATED("created"),
 
         /**
          * This update is generated when a ticket is closed.
          */
-        CLOSED,
+        CLOSED("closed"),
 
         /**
          * This update is generated when a ticket is reopened.
          */
-        REOPENED,
+        REOPENED("reopened"),
 
         /**
          * This update is generated when a ticket is claimed.
          */
-        CLAIMED,
+        CLAIMED("claimed"),
 
         /**
          * This update is generated when a ticket is unclaimed.
          */
-        UNCLAIMED,
+        UNCLAIMED("unclaimed"),
 
         /**
          * This update is generated when a ticket is escalated to another tier.
          */
-        ESCALATED
+        ESCALATED("escalated");
+
+        /**
+         * The ID of the type.
+         */
+        private @Getter final String id;
     }
 }
